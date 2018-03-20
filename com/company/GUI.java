9374d371dc058adb8e97f39b4651420e1ba82206
adilahmed1997 <32398945+adilahmed1997@users.noreply.github.com>
2018-03-20 19:08:20 +0000

    Started work on the GUI class

---
+++ com/company/GUI.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class GUI {
+    JFrame jFrame;
+    ArrayList<ImageIcon> icons;
+    ArrayList<JButton> buttons;
+    JPanel jPanel;
+
+    public GUI()
+    {
+    create();
+
+    }
+
+    private void create()
+    {
+        jFrame = new JFrame(); // Create jFrame blank window
+        jPanel = new JPanel();
+        jFrame.setVisible(true);
+        jFrame.setTitle("Slider puzzle game");
+        jFrame.setSize(320,475);
+        jPanel.setLayout(new GridLayout(3,4));
+        jFrame.add(jPanel);
+
+        createButtons();
+    }
+
+    private void createButtons()
+    {
+        ArrayList<ImageIcon> icons = new ArrayList<ImageIcon>();
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
+
+        int i=0;
+        for(int x = 0; x<3; x++){
+            for (int y = 0; y<4; y++){
+                icons.add(new ImageIcon("bart"+i+".jpg"));
+                buttons.add(new JButton(icons.get(i)));
+
+
+                jPanel.add(buttons.get(i));
+                System.out.println(buttons.get(i).getText());
+
+                i++;
+            }
+
+        }
+        jFrame.pack();
+
+    }
+}
